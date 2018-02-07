e6d0a663365401a1cd1b57a97c6b0e18ff8d35e9
aruss <arusslabs@gmail.com>
2018-02-07 16:03:03 +0100

    Added source code

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # updeps
-Uses your csproj file to update nuspec dependencies.
+Uses your csproj file (or multiple) to update nuspec dependencies.
+
+Image you have a project `Foo.csproj` and you created a custom `Foo.nuspec`. You do it by your self because the automated generation of nuspec files in Visual Studio actually sucks. So, now you always forgetting to update your dependencies. With updeps you can update your nuspec file with just one following command. 
+
+```
+updeps Foo.csproj Foo.nuspec
+```
+
+Or in case you want to combine multiple project in one nuspec file then run following
+
+```
+updeps Foo.csproj Bar.csproj Foo.nuspec
+```
+
+It will output something like this
+
+```
+Reading C:\Projects\Foo.csproj
+  Added 2.0.1 Microsoft.AspNetCore.Authorization
+  Added 2.0.1 Microsoft.AspNetCore.Hosting.Abstractions
+  Added 2.0.1 Microsoft.AspNetCore.WebUtilities
+  Added 2.0.0 Microsoft.Extensions.Configuration
+  Added 2.0.0 Microsoft.Extensions.Configuration.Binder
+  Added 2.0.0 Microsoft.Extensions.Configuration.CommandLine
+  Added 2.0.0 Microsoft.Extensions.Configuration.EnvironmentVariables
+  Added 2.0.0 Microsoft.Extensions.Configuration.FileExtensions
+  Added 2.0.0 Microsoft.Extensions.Configuration.Json
+```
+
+And if you hook it up to MSBuild then Visual Studio will do it automatically for you on every build. But for me, I hooked it into my NuGet publishing script. 
+
+
---
+++ src/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System;
+using System.IO;
+using System.Xml;
+
+namespace updeps
+{
+    class Reference
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string ProjectFilePath { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Require at least two arguments");
+                Environment.Exit(2);
+            }
+
+            FileInfo[] fileInfos = new FileInfo[args.Length];
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                FileInfo fileInfo = new FileInfo(args[i]);
+
+                if (!fileInfo.Exists)
+                {
+                    Console.WriteLine(
+                        "File {0} does not exists", fileInfo.FullName);
+
+                    Environment.Exit(2);
+                }
+
+                fileInfos[i] = fileInfo;
+            }
+
+            string[] csprojFilePathes = fileInfos
+                .Take(fileInfos.Length - 1)
+                .Select(s =>
+                {
+                    if (s.Extension != ".csproj")
+                    {
+                        Console
+                            .WriteLine("Only *.csproj files are supported.");
+
+                        Environment.Exit(2);
+                    }
+
+                    return s.FullName;
+                })
+                .ToArray();
+
+
+            string nuspecFilePath = fileInfos
+                .Skip(fileInfos.Length - 1)
+                .Take(1)
+                .Select(s =>
+                {
+                    if (s.Extension != ".nuspec")
+                    {
+                        Console
+                            .WriteLine("Last argument must be a .nuspec file.");
+
+                        Environment.Exit(2);
+                    }
+
+                    return s.FullName;
+                }).First();
+
+          
+            IEnumerable<Reference> references =
+                GetReferences(csprojFilePathes);
+
+            UpdateNuspecFile(nuspecFilePath, references);
+            
+            Environment.Exit(0);
+        }
+
+        private static void UpdateNuspecFile(
+			string nuspecFilePath, 
+			IEnumerable<Reference> references)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(nuspecFilePath);
+
+            // TODO: work something out if you require targeting different frameworks 
+            // Mc'Giver would be proud!
+            var node = doc.SelectNodes("//*[local-name()='package']").Item(0);
+            node = node.SelectNodes("//*[local-name()='metadata']").Item(0);
+            node = node.SelectNodes("//*[local-name()='dependencies']").Item(0);
+            node = node.SelectNodes("//*[local-name()='group']").Item(0);
+
+            while (node.HasChildNodes)
+            {
+                node.RemoveChild(node.FirstChild);
+            }
+
+            foreach (var reference in references)
+            {
+                XmlElement elem = doc.CreateElement("dependency",
+                    "http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd");
+
+                elem.SetAttribute("id", reference.Name);
+                elem.SetAttribute("version", reference.Version);
+                elem.SetAttribute("exclude", "Build,Analyzers");
+                node.AppendChild(elem);
+            }
+
+            doc.Save(nuspecFilePath);
+        }
+
+        static IEnumerable<Reference> GetReferences(
+            string[] csprojFilePathes)
+        {
+            Dictionary<string, Reference> references =
+                            new Dictionary<string, Reference>();
+
+            foreach (string csprojFilePath in csprojFilePathes)
+            {
+                Console.WriteLine("Reading {0}", csprojFilePath);
+
+                foreach (Reference reference in GetReferences(csprojFilePath))
+                {
+                    if (references.ContainsKey(reference.Name))
+                    {
+                        Reference referenceAdded = references[reference.Name];
+
+                        if (referenceAdded.Version != reference.Version)
+                        {
+                            Console.WriteLine(
+                                "  Failed {0} {1}",
+                                reference.Version,
+                                reference.Name);
+
+                            Console.WriteLine(
+                                "    Different versions of the same package reference.");
+
+                            Console.WriteLine(
+                                "    {0} in {1}",
+                                referenceAdded.Version,
+                                referenceAdded.ProjectFilePath);
+
+                            Environment.Exit(1);
+                        }
+                        else
+                        {
+                            Console.WriteLine("  Skipped {0} {1}",
+                               reference.Version, reference.Name);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("  Added {0} {1}",
+                            reference.Version, reference.Name);
+
+                        references.Add(reference.Name, reference);
+                    }
+                }
+            }
+
+            return references.Values;
+        }
+
+        static IEnumerable<Reference>
+            GetReferences(string csprojFilePath)
+        {
+            XDocument doc = XDocument.Load(csprojFilePath);
+            XElement project = doc.Element("Project");
+
+            foreach (var itemGroup in project.Elements("ItemGroup"))
+            {
+                foreach (var packageReference in itemGroup
+                    .Elements("PackageReference"))
+                {
+                    yield return new Reference
+                    {
+                        Name = packageReference.Attribute("Include").Value,
+                        Version = packageReference.Attribute("Version").Value,
+                        ProjectFilePath = csprojFilePath
+                    };
+                }
+            }
+        }
+    }
+}
